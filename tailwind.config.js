7ca22843f813e4a47667871c33e8e8892f2eb8d5
Oscar <oscarrc.web@gmail.com>
2023-02-23 13:00:34 +0100

    [ADD] Themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,84 +9,84 @@ module.exports = {
       themes: [
         {
           classic:{
-            "primary": "#161A1A", // D-Pad
-            "secondary": "#636B7B", // Start - Select
-            "accent": "#782157", // A / B
-            "neutral": "#BDBEC3", // Shell
-            "neutral-content": "#782157", // Game Boy Text
-            "base-100": "#3D3E73", // Line 1     
-            "base-200": "#782157", // Line 2
-            "base-300": "" // Screen frame
+            "primary": "#070C0B", // D-Pad
+            "secondary": "#777F87", // Start - Select
+            "accent": "#7B2358", // A / B
+            "neutral": "#BABCC1", // Shell
+            "neutral-content": "#414477", // Game Boy Text
+            "base-100": "#414477", // Line 1     
+            "base-200": "#7B2358", // Line 2
+            "base-300": "#626A7B" // Screen frame
           },
           yellow:{
-            "primary": "#161A1A",
-            "secondary": "#161A1A",
-            "accent": "#161A1A",
-            "neutral": "#E4AC00",
-            "neutral-content": "#5E6C01",
+            "primary": "#232129",
+            "secondary": "#232129",
+            "accent": "#232129",
+            "neutral": "#F1B32F",
+            "neutral-content": "#232129",
             "base-100": "#3D3E73",            
             "base-200": "#782157",
             "base-300": "#212121"  
           },
           red:{
-            "primary": "#161A1A",
-            "secondary": "#161A1A",
-            "accent": "#161A1A",
+            "primary": "#232129",
+            "secondary": "#232129",
+            "accent": "#232129",
             "neutral": "#E22E32",
-            "neutral-content": "#5E6C01",
+            "neutral-content": "#232129",
             "base-100": "#3D3E73",            
             "base-200": "#782157",
-            "base-300": ""  
+            "base-300": "#212121"
           },
           black:{
-            "primary": "#161A1A",
-            "secondary": "#161A1A",
-            "accent": "#161A1A",
+            "primary": "#525457",
+            "secondary": "#525457",
+            "accent": "#525457",
             "neutral": "#000000",
             "neutral-content": "#782157",
             "base-100": "#3D3E73",            
             "base-200": "#782157",
-            "base-300": ""  
+            "base-300": "#626A7B"
           },
           white:{
-            "primary": "#161A1A",
+            "primary": "#000000",
             "secondary": "#161A1A",
-            "accent": "#161A1A",
+            "accent": "#000000",
             "neutral": "#ffffff",
-            "neutral-content": "#5E6C01",
+            "neutral-content": "#000000",
             "base-100": "#3D3E73",            
             "base-200": "#782157",
-            "base-300": ""  
+            "base-300": "#000000"  
           },
           blue:{
-            "primary": "#161A1A",
-            "secondary": "#161A1A",
-            "accent": "#161A1A",
+            "primary": "#232129",
+            "secondary": "#232129",
+            "accent": "#232129",
             "neutral": "#154D9A",
-            "neutral-content": "#5E6C01",
+            "neutral-content": "#232129",
             "base-100": "#3D3E73",            
             "base-200": "#782157",
-            "base-300": ""  
+            "base-300": "#212121"
           },
           green:{
-            "primary": "#161A1A",
-            "secondary": "#161A1A",
-            "accent": "#161A1A",
+            "primary": "#232129",
+            "secondary": "#232129",
+            "accent": "#232129",
             "neutral": "#00B67D",
-            "neutral-content": "#5E6C01",
+            "neutral-content": "#232129",
             "base-100": "#3D3E73",            
             "base-200": "#782157",
-            "base-300": ""  
+            "base-300": "#212121"
           },
           transparent:{
-            "primary": "#161A1A",
-            "secondary": "#161A1A",
-            "accent": "#161A1A",
+            "primary": "#232129",
+            "secondary": "#232129",
+            "accent": "#232129",
             "neutral": "transparent",
-            "neutral-content": "#5E6C01",
+            "neutral-content": "#D90D32",
             "base-100": "#3D3E73",            
             "base-200": "#782157",
-            "base-300": ""  
+            "base-300": "#212121"
           }
         }
       ]
