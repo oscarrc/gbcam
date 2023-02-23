445222c7ec43e49ebf565b3278870d5f9c3d2d68
Oscar <oscarrc.web@gmail.com>
2023-02-23 17:02:22 +0100

    [MOD] Font Names [ADD] Header elements [ADD] GBIcon

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,29 @@
+import { BsChevronDown } from "react-icons/bs";
+
+const MODELS = ["Yellow", "Red", "Black", "White", "Blue", "Green", "Transparent"];
+
 const Header = () => {
     return (
-        <header></header>
+        <header>
+            <div class="navbar fixed w-full top-0 left-0 bg-neutral text-neutral-content border-b-2 border-primary border-opacity-20">
+                <div class="flex-1">
+                    <a href="/" class="btn btn-ghost normal-case text-4xl font-title italic">GBCam</a>
+                </div>
+                <div class="flex-none font-text">
+                    <ul class="dropdown dropdown-end">
+                        <li tabindex="0">
+                            <label role="button" aria-label="Model" className="flex items-center gap-2 mx-4">Model<BsChevronDown className="inline h-2 w-2" /></label>
+                            <div class="dropdown-content menu shadow w-40 bg-neutral">
+                                <li><button className="normal-case">Classic DMG</button></li>
+                                {
+                                    MODELS.map(m => <li><button className="normal-case">{m}</button></li> )
+                                }
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ module.exports = {
     extend: {
       minHeight:{
         "dscreen": "100dvh"
+      },
+      fontFamily:{
+        "title": ["Gil Sans"],
+        "button": ["Nes Controller"],
+        "brand": ["Pretendo"],
+        "text": ["Univers"]
+      },
+      colors:{
+        "dmg": "#BABCC1",
+        "yellow": "#F1B32F",
+        "red": "#E22E32",
+        "black": "#000000",
+        "white": "#ffffff",
+        "blue": "#154D9A",
+        "green": "#00B67D",
+        "transparent": "#0f380f",
+        "line-1": "#7B2358",
+        "line-2": "#3D3E73"
       }
     }
   },
@@ -13,84 +31,76 @@ module.exports = {
       themes: [
         {
           classic:{
+            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
             "primary": "#070C0B", // D-Pad
             "secondary": "#777F87", // Start - Select
             "accent": "#7B2358", // A / B
             "neutral": "#BABCC1", // Shell
             "neutral-content": "#414477", // Game Boy Text
-            "base-100": "#414477", // Line 1     
-            "base-200": "#7B2358", // Line 2
-            "base-300": "#626A7B" // Screen frame
+            "base-100": "#626A7B" // Screen frame
           },
           yellow:{
+            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
             "primary": "#232129",
             "secondary": "#232129",
             "accent": "#232129",
             "neutral": "#F1B32F",
             "neutral-content": "#232129",
-            "base-100": "#3D3E73",            
-            "base-200": "#782157",
-            "base-300": "#212121"  
+            "base-100": "#212121"  
           },
           red:{
+            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
             "primary": "#232129",
             "secondary": "#232129",
             "accent": "#232129",
             "neutral": "#E22E32",
             "neutral-content": "#232129",
-            "base-100": "#3D3E73",            
-            "base-200": "#782157",
-            "base-300": "#212121"
+            "base-100": "#212121"
           },
           black:{
+            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
             "primary": "#525457",
             "secondary": "#525457",
             "accent": "#525457",
             "neutral": "#000000",
             "neutral-content": "#782157",
-            "base-100": "#3D3E73",            
-            "base-200": "#782157",
-            "base-300": "#626A7B"
+            "base-100": "#626A7B"
           },
           white:{
+            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
             "primary": "#000000",
             "secondary": "#161A1A",
             "accent": "#000000",
             "neutral": "#ffffff",
             "neutral-content": "#000000",
-            "base-100": "#3D3E73",            
-            "base-200": "#782157",
-            "base-300": "#000000"  
+            "base-100": "#000000"  
           },
           blue:{
+            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
             "primary": "#232129",
             "secondary": "#232129",
             "accent": "#232129",
             "neutral": "#154D9A",
             "neutral-content": "#232129",
-            "base-100": "#3D3E73",            
-            "base-200": "#782157",
-            "base-300": "#212121"
+            "base-100": "#212121"
           },
           green:{
+            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
             "primary": "#232129",
             "secondary": "#232129",
             "accent": "#232129",
             "neutral": "#00B67D",
             "neutral-content": "#232129",
-            "base-100": "#3D3E73",            
-            "base-200": "#782157",
-            "base-300": "#212121"
+            "base-100": "#212121"
           },
           transparent:{
+            ...require("daisyui/src/colors/themes")["[data-theme=black]"],
             "primary": "#232129",
             "secondary": "#232129",
             "accent": "#232129",
             "neutral": "transparent",
             "neutral-content": "#D90D32",
-            "base-100": "#3D3E73",            
-            "base-200": "#782157",
-            "base-300": "#212121"
+            "base-100": "#212121"
           }
         }
       ]
