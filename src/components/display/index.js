d11d836407e7bc207b0d46dbab4150f0c1a3b6d7
Oscar <oscarrc.web@gmail.com>
2023-02-24 13:19:31 +0100

    [WIP] Display responsiveness

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -20,7 +20,7 @@ const Display = ({ className }) => {
         <div className={`aspect-4/3 ${className} bg-base-100 rounded-lg rounded-br-[2rem] flex flex-col gap-2 py-2`}>
             <div className="flex flex-row items-center gap-4 px-4">
                 <span className="flex-1 h-2 border-y-2 border-t-line-1 border-b-line-2"></span>
-                <p className="text-white">Made with <AiFillHeart className="inline h-4 w-4" /> by <a href="https://oscarrc.me" target="_BLANK" rel="noreferrer noopener">Oscar R.C.</a></p>                
+                <p className="text-white text-sm">Made with <AiFillHeart className="inline h-3 w-3" /> by <a href="https://oscarrc.me" target="_BLANK" rel="noreferrer noopener">Oscar R.C.</a></p>                
                 <span className="h-2 min-w-[10%] border-y-2 border-t-line-1 border-b-line-2"></span>
             </div>
             <div className="flex flex-row items-start justify-center pb-10 pr-20">
@@ -28,7 +28,7 @@ const Display = ({ className }) => {
                     <span className="h-2 w-2 bg-red rounded-full bg-opacity-20"></span>
                     <span className="text-white text-xs relative -bottom-px">Camera</span>
                 </div>
-                <canvas ref={canvasRef} className="bg-display w-full max-w-[calc(100% - 10rem)]">
+                <canvas ref={canvasRef} className="bg-display w-full">
 
                 </canvas>
             </div>
