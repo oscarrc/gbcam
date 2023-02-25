3e134f70c73cd16b403b17dff20c0f0dfd916352
Oscar <oscarrc.web@gmail.com>
2023-02-25 12:33:36 +0100

    [MOD] Header responsiveness

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
         <header>
             <div className="navbar w-full fixed top-0 left-0 bg-neutral text-neutral-content rounded-t-2xl border-b-2 border-primary border-opacity-20 min-h-12 py-0 px-12 z-20">
                 <div className="flex-1">
-                    <a href="/" className="btn btn-ghost normal-case text-4xl font-title italic">GBCam</a>
+                    <a href="/" className="btn btn-ghost normal-case text-2xl sm:text-4xl font-title italic">GBCam</a>
                 </div>
                 <div className="flex-none font-text">
                     <ul className="dropdown dropdown-end">
