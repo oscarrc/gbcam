68841f76a9205eb431dba12c2120a193588dc828
Oscar <oscarrc.web@gmail.com>
2023-02-28 12:38:09 +0100

    [DEL] unnecesary router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
-
 import Layout from './components/layout';
-
-const Main = lazy(() => import('./views/Main'));
+import Main from './views/Main';
+import { CameraProvider } from "./hooks/useCamera";
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      element: <Layout />,
-      children: [
-        {
-          id: "main",
-          path: "/",
-          element: <Main />
-        }
-      ]
-    }
-  ])
-
   return (
-    <Suspense>
-      <RouterProvider router={router} />
-    </Suspense>
+    <CameraProvider>
+      <Layout>
+        <Main />
+      </Layout>
+    </CameraProvider>
   );
 }
 
