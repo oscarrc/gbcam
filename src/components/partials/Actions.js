93024a367e7e7cb8439924e7896fffb6e605cb41
Oscar <oscarrc.web@gmail.com>
2023-02-28 11:40:01 +0100

    [ADD] Share

--- src/components/partials/Actions.js
+++ src/components/partials/Actions.js
@@ -1,7 +1,20 @@
 import { useCamera } from "../../hooks/useCamera";
 
 const Mode = () => {
-    const { save } = useCamera();
+    const { save, recording, snapshot } = useCamera();
+
+    const share = () => {
+        const media = snapshot ? snapshot : recording;
+        const extension = snapshot ? ".jpg" : ".mp4";
+
+        if(!media) return;
+        navigator.share({
+            url: "https://gbCam.oscarrc.me",
+            files: [
+                new File([media], `${Date.now()}${extension}`)
+            ]
+        })
+    }
 
     return (
         <div className="grid grid-cols-2 gap-4 font-bold text-neutral-content text-xs">
@@ -10,7 +23,7 @@ const Mode = () => {
                 <label htmlFor="Save" className="text-button text-center block">SAVE</label>
             </span>
             <span className="-rotate-20">
-                <button name="Share" aria-label="Share" className="btn btn-secondary p-0 min-h-0 h-4 w-12 rounded-lg shadow-lg"></button>
+                <button onClick={share} name="Share" aria-label="Share" className="btn btn-secondary p-0 min-h-0 h-4 w-12 rounded-lg shadow-lg"></button>
                 <label htmlFor="Share" className="text-button text-center block">SHARE</label>
             </span>
         </div>
