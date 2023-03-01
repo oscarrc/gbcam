fcdd2142bb69f5da592df6170b49497a8e6129bc
Oscar <oscarrc.web@gmail.com>
2023-03-01 13:17:15 +0100

    [MOD] Handle window orientation / aspect ratio

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,17 +1,41 @@
 import { Actions, Brand, Controls, Snap } from "../components/partials";
+import { useEffect, useState } from "react";
 
 import Display from "../components/display";
 
 const Main = () => {
+    const [ landscape, setLandscape ] = useState(false);
+
+    useEffect(() => {
+        if (typeof window === 'undefined') return;
+        const checkOrientation = () => setLandscape(window.innerWidth > window.innerHeight);
+
+        checkOrientation();
+        window.addEventListener("resize", checkOrientation);
+
+        return () => window.removeEventListener("resize", checkOrientation);
+    }, [])
+
     return (
-        <section className="flex flex-col flex-1 items-center justify-around px-12 md:max-w-display mt-24 mx-auto">
-            <div className="">
-                <Display />
-                <Brand />
-            </div>
+        <section className="flex flex-col flex-1 items-center justify-around px-12 mt-24 mx-auto">
+            {
+                !landscape && 
+                <div className="">
+                    <Display />
+                    <Brand />
+                </div>
+            }
             <div className="flex flex-col flex-1 justify-around items-center w-full">
-                <div className="flex justify-between items-center w-full px-4">
+                <div className="flex justify-between items-center w-full px-4  gap-16">
                     <Controls />
+                    {
+                        landscape &&
+                        <div className="flex-1">
+                            <Display />
+                            <Brand />
+                            {landscape}
+                        </div>
+                    }
                     <Snap />
                 </div>
                 <Actions />
