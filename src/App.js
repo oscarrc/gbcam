d9f7c5f77cb44a1944efa2b1a9276fe708f9016d
Oscar <oscarrc.web@gmail.com>
2023-03-02 12:26:37 +0100

    [DEL] Test icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { CamIcon } from "./components/partials";
 import { CameraProvider } from "./hooks/useCamera";
 import Layout from './components/layout';
 import Main from './views/Main';
+
 const App = () => {
   return (
     <CameraProvider>
       <Layout>
         <Main />
-        <CamIcon className="yellow" selfie={false} />
       </Layout>
     </CameraProvider>
   );
