43df8f311fba8a7d9a51e0d1a7668cd515835433
Oscar <oscarrc.web@gmail.com>
2023-03-03 09:12:33 +0100

    [ADD] Handle touchscreen events on snap

--- src/components/partials/Snap.js
+++ src/components/partials/Snap.js
@@ -7,26 +7,48 @@ const Snap = () => {
     const { clear, startRecording, stopRecording, takeSnapshot, snapshot, recording } = useCamera();
     const start = useRef(0);
     const timer = useRef(null);
+    const snap = useRef(null);
 
-    // TODO: touchscreen events
+    const checkTouches = (touches) => {
+        let isTouching = false;
+
+        Object.values(touches).forEach(t => {
+            let elm = document.elementFromPoint(t.pageX,t.pageY)
+            if(elm === snap.current) isTouching = true;
+        })
+
+        return isTouching;
+    }
+
     const handleStart = (e) => {
         if(snapshot || recording) return;
+        if (e.type === "touchstart" && !checkTouches(e.touches)) return;
+        
         start.current = Date.now();
         timer.current = setTimeout(() => { startRecording() }, 500)
     }
 
     const handleStop = (e) => {
         if(snapshot || recording) return;
+        if (e.type === "touchstart" && checkTouches(e.touches)) return;
+
         if(Date.now() - start.current < 500 ){
             clearTimeout(timer.current);
             takeSnapshot();
         }else stopRecording();
     }
 
+    const events = {
+        onMouseDown: handleStart,
+        onMouseUp: handleStop,
+        onTouchStart: handleStart,
+        onTouchEnd: handleStop
+    }
+
     return (
         <div className="grid grid-cols-2 grid-rows-2 rotate-35 text-button font-bold text-neutral-content text-xs">            
             <div className="text-center col-start-2  -rotate-35">
-                <button onMouseDown={handleStart} onMouseUp={handleStop} aria-label="Take photo or video" className="btn btn-circle shadow-lg btn-accent">
+                <button ref={snap} { ...events } aria-label="Take photo or video" className="btn btn-circle shadow-lg btn-accent">
                     <BsFillCameraFill/>
                 </button>
                 <label className="block ml-8 -rotate-35">A</label>
